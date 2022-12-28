028d250bdb1d2a9c52511e2b896b047d91f25527
Nicolas Jativa <nicojativa98@gmail.com>
2022-12-28 15:42:53 -0500

    Add Hashing and Salting

--- app.js
+++ app.js
@@ -14,11 +14,15 @@ app.use(express.static("public")); //Render local files
 const mongoose = require("mongoose"); //Mongoose 
 mongoose.set("strictQuery", false);
 
-const encrypt = require("mongoose-encryption");
+//const encrypt = require("mongoose-encryption"); - Alternative encryption method(Not secure)
+
+//const SHA256 = require("crypto-js/sha256"); //Hashing Passwords with SHA256
+
+const bcrypt = require('bcrypt'); //Hashing and Salting Passwords with bcrypt
+const saltRounds = 10;
 
 main().catch((err) => console.log(err)); //Catch main Errors
 
-console.log(process.env.API_KEY);
 
 /* * * * * * * Code Start * * * * * * */
 
@@ -38,38 +42,34 @@ async function main() {
         password: String
     });
 
-  
-
-    userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
-
     //Create Users collection
     const User = mongoose.model('User', userSchema);
 
     //Register Users
-    app.post("/register", function(req, res){
+    app.post("/register", function (req, res) {
 
-        const userEmail = req.body.username; //Getting user email
-        const userPassword = req.body.password; //Getting user pw
+      bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
 
-        const newUser = new User ({ //Creating a new user with the new data
-            email: userEmail,
-            password: userPassword
+        const newUser = new User({ //Creating a new user with the new data and hashed pw.
+          email: req.body.username,
+          password: hash,
         });
 
-        newUser.save(function(err){ //Saving the user in the db
-            if(!err){
-                res.render('secrets');
-            } else {
-                console.log(err);
-            }
-        })
-    })
+        newUser.save(function (err) { //Saving the user in the db
+          if (!err) {
+            res.render("secrets");
+          } else {
+            console.log(err);
+          }
+        });
+      });
+    });
 
     //Login Users
     app.post("/login", function (req, res){
 
         const loginEmail = req.body.username; //Getting user email
-        const loginPassword = req.body.password; //Getting user pw
+        const loginPassword = req.body.password; //Getting user Hashed Password
 
         User.findOne({email: loginEmail}, function(err, foundUser){ //Looking for matching user email and pw.
             if(err){
@@ -77,16 +77,19 @@ async function main() {
                 res.send("Wrong email please try again.")                
             } else {
                 if(foundUser){
-                    if(foundUser.password === loginPassword){
-                        res.render('secrets'); //Render Secrets if credentials match
-                    } else {
-                        console.log(err);
-                        res.send("Wrong password, please try again.");   
-                    }
+                    bcrypt.compare(loginPassword, foundUser.password, function(err, result) {
+                        if(result === true){
+                            res.render('secrets'); //Render Secrets if credentials match
+                        } else {
+                            console.log(err);
+                            res.send("Wrong password, please try again.");
+                        }
+                    });
                 }
             }
         });
     });
+ 
 
 
 
